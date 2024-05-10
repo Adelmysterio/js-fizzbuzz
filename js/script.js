b907a5fb91417baf2f60de8ff98fdf0a2415d41a
AdelMysterio <storliniadelmo@gmail.com>
2024-05-10 14:05:18 +0200

    added ul on html and his li with js related to the results of the program written

--- js/script.js
+++ js/script.js
@@ -4,21 +4,33 @@ for (let i = 1; i <= 100; i++) {
     /* Definisco le variabili const fizz e buzz usando il quoziente */
     const fizz = (i % 3 == 0);
     const buzz = (i % 5 == 0);
-    
+
     /* Pongo le condizioni */
     if (fizz && buzz) {
         console.log('FizzBuzz')
+        const ulElem = document.querySelector('ul')
+        const liElem = document.createElement('li')
+        ulElem.appendChild(liElem).innerHTML = "FizzBuzz"
     }
 
     else if (fizz) {
         console.log('fizz')
+        const ulElem = document.querySelector('ul')
+        const liElem = document.createElement('li')
+        ulElem.appendChild(liElem).innerHTML = "Fizz"
     }
 
     else if (buzz) {
         console.log('buzz')
+        const ulElem = document.querySelector('ul')
+        const liElem = document.createElement('li')
+        ulElem.appendChild(liElem).innerHTML = "Buzz"
     }
 
     else {
         console.log(i)
+        const ulElem = document.querySelector('ul')
+        const liElem = document.createElement('li')
+        ulElem.appendChild(liElem).innerHTML = i
     }
 }
