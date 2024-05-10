5059f22829a3114b37ee087fd00537f8b9979e61
AdelMysterio <storliniadelmo@gmail.com>
2024-05-10 13:15:49 +0200

    corrected pseudocode and created fizz and buzz const

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
+/* Creo un ciclo for che scriva i numeri da 1 a 100 */
 for (i=1; i<=100; i++) {
-    console.log (i)
+
+    /* definisco le variabili const fizz e buzz usando il quoziente */
+    const fizz = (i % 3 == 0);
+    const buzz = (i % 5 == 0);
+
     
+    
+
+    console.log (i)
+
 }
