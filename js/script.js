d602d36c0ad2e95e0f4c482b2a0a501a93a50c9f
AdelMysterio <storliniadelmo@gmail.com>
2024-05-13 09:44:45 +0200

    improved semantic in js

--- js/script.js
+++ js/script.js
@@ -4,36 +4,30 @@ for (let i = 1; i <= 100; i++) {
     /* Definisco le variabili const fizz e buzz usando il quoziente */
     const fizz = (i % 3 == 0);
     const buzz = (i % 5 == 0);
+    const ulElem = document.querySelector('ul')
+    const liElem = document.createElement('li')
 
     /* Pongo le condizioni */
     if (fizz && buzz) {
         console.log('FizzBuzz')
-        const ulElem = document.querySelector('ul')
-        const liElem = document.createElement('li')
         ulElem.appendChild(liElem).innerHTML = "FizzBuzz"
         liElem.classList.add('bg-warning')
     }
 
     else if (fizz) {
         console.log('fizz')
-        const ulElem = document.querySelector('ul')
-        const liElem = document.createElement('li')
         ulElem.appendChild(liElem).innerHTML = "Fizz"
         liElem.classList.add('bg-danger')
     }
 
     else if (buzz) {
         console.log('buzz')
-        const ulElem = document.querySelector('ul')
-        const liElem = document.createElement('li')
         ulElem.appendChild(liElem).innerHTML = "Buzz"
         liElem.classList.add('bg-primary')
     }
 
     else {
         console.log(i)
-        const ulElem = document.querySelector('ul')
-        const liElem = document.createElement('li')
         ulElem.appendChild(liElem).innerHTML = i
         liElem.classList.add('fw-bold')
     }
